d6ff5f48e07d6a1eebab53d513e3a72d729309d0
Paulo Black <paulo@pauloblack.com>
2024-10-23 09:50:30 -0400

    testing multiple versions with default version install

--- Formula/preen@0.2.2.rb
+++ Formula/preen@0.2.2.rb
@@ -1,8 +1,8 @@
-class Preen < Formula
+class PreenAT022 < Formula
   desc "Local-first federated analytics query engine."
   homepage "https://github.com/preendata/preen"
-  url "https://github.com/preendata/preen/releases/download/v0.2.3/preen-darwin_arm64-v0.2.3.tar.gz"
-  sha256 "2f897276da29bcb0df3a2886476f0c5e6c86b9a8ed5821d76889b801df138e41"
+  url "https://github.com/preendata/preen/releases/download/v0.2.2/preen-darwin_arm64-v0.2.2.tar.gz"
+  sha256 "8158e94b81f9a37cb0abc307aa7dfb5d7c6179ac1e70201dec3f53d311307e89"
   license "LGPL 3.0"
 
   def install
